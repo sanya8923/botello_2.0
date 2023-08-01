5108f1ec9abc7823840809119a1e846fc4549f62
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-01 22:09:51 +0400

    add main

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 from bot import bot
+from dp import dp
+import logging
+import asyncio
+from handlers import on_new_message_group_supergroup
+from colorama import init, Fore
+
+
+async def main():
+    init(autoreset=True)
+    logging.basicConfig(level=logging.INFO, format=Fore.YELLOW + '%(message)s')
+
+    dp.include_routers(
+        on_new_message_group_supergroup.router
+    )
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=['message', 'callback_query', 'chat_member', 'my_chat_member'])
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
 
--- other.py
+++ other.py
@@ -4,5 +4,5 @@ from colorama import init, Fore
 
 async def name_method(func: Callable):
     init(autoreset=True)
-    print(Fore.BLUE + f'{func.__class__.__name__}.{func.__name__}')
+    print(Fore.BLUE + f'{func.__name__}')
     return func
