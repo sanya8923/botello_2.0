a6a5fcf9d4de8a29c906930455e7bc6af7a2af76
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-01 22:34:51 +0400

    delete abstract methods from class Object

--- models/objects/object.py
+++ models/objects/object.py
@@ -5,13 +5,3 @@ from abc import ABC, abstractmethod
 class Object(ABC):
     def __init__(self):
         self.id: Optional[int] = None
-
-    @abstractmethod
-    async def to_json(self):
-        pass
-
-    @abstractmethod
-    async def from_json(self):
-        pass
-
-
