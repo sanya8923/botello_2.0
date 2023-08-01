43c7c8eaa6588f9b55b91b0b3fd5f1cd6309d85a
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-01 22:55:58 +0400

    add methods

--- models/serializer/chat_serializer.py
+++ models/serializer/chat_serializer.py
@@ -1,5 +1,5 @@
 from serializer import Serializer
-from models.objects.chat import Chat, PublicChat, PrivateChat
+from models.objects.chat import Chat, PublicChat, PrivateChat, Group, SuperGroup
 from other import print_name_method
 
 
@@ -9,22 +9,50 @@ class ChatSerializer(Serializer):
         self.chat = chat
         self.id = chat.id
 
+    async def to_json(self):  # abstract method
+        pass
+
+    async def from_json(self):  # abstract method
+        pass
+
 
 class PrivateChatSerializer(ChatSerializer):
+    @print_name_method
     def __init__(self, chat: PrivateChat):
         super().__init__(chat)
 
+    @print_name_method
+    async def to_json(self):  # TODO: add method
+        pass
+
+    @print_name_method
+    async def from_json(self):  # TODO: add method
+        pass
+
 
 class PublicChatSerializer(ChatSerializer):
+    @print_name_method
     def __init__(self, chat: PublicChat):
         super().__init__(chat)
         self.username = chat.username
         self.title = chat.title
 
     @print_name_method
-    async def to_json(self):
+    async def to_json(self):  # TODO: add method
         pass
 
     @print_name_method
-    async def from_json(self):
+    async def from_json(self):  # TODO: add method
         pass
+
+
+class GroupSerializer(PublicChatSerializer):
+    @print_name_method
+    def __init__(self, chat: Group):
+        super().__init__(chat)
+
+
+class SuperGroupSerializer(PublicChatSerializer):
+    def __init__(self, chat: SuperGroup):
+        super().__init__(chat)
+
