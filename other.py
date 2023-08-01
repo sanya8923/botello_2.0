cd13cfad9d1ada990ad2dc910ad7405f2b141ae9
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-01 22:13:32 +0400

    rename name_method to print_name_method

--- other.py
+++ other.py
@@ -2,7 +2,7 @@ from typing import Callable
 from colorama import init, Fore
 
 
-async def name_method(func: Callable):
+async def print_name_method(func: Callable):
     init(autoreset=True)
     print(Fore.BLUE + f'{func.__name__}')
     return func
