9ccd637adf29db9d0a85d0bbf0f9e895b3b7a177
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-02 22:25:20 +0400

    add method add_to_json to class Serializer

--- models/serializer/serializer.py
+++ models/serializer/serializer.py
@@ -1,5 +1,10 @@
 from models.objects.object import Object
 from abc import ABC, abstractmethod
+from colorama import init, Fore
+from typing import IO
+import json
+
+init(autoreset=True)
 
 
 class Serializer(ABC):
@@ -13,3 +18,9 @@ class Serializer(ABC):
     @abstractmethod
     async def from_json(self):
         pass
+
+    async def add_to_json(self, json_object: str, data: dict):
+        print(Fore.LIGHTYELLOW_EX + f'{self.add_to_json.__name__} in class {self.__class__.__name__}')
+
+        with open(f'{json_object}', 'w') as file:
+            json.dump(data, file, separators=(',\n', ': '))
