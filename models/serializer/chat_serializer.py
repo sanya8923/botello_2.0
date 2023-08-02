bbce51d1dcae6ec048b8f3a97d4dd56c9b171254
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-02 19:37:24 +0400

    refactor method to_json in class PublicChatSerializer

--- models/serializer/chat_serializer.py
+++ models/serializer/chat_serializer.py
@@ -47,7 +47,13 @@ class PublicChatSerializer(ChatSerializer):
 
     async def to_json(self):
         print(Fore.LIGHTYELLOW_EX + f'{self.to_json.__name__} in class {self.__class__.__name__}')
-        pass
+        data = {
+            'id': self.chat_id,
+            'username': self.username,
+            'title': self.title,
+            'type': self.chat_type
+        }
+        return data
 
     async def from_json(self):  # TODO: add method
         print(Fore.LIGHTYELLOW_EX + f'{self.from_json.__name__} in class {self.__class__.__name__}')
