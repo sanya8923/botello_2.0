e57d283b173331a8c47758039a634d59efc3bcf0
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-03 22:40:40 +0400

    refactor user.py

--- models/objects/user.py
+++ models/objects/user.py
@@ -1,52 +1,53 @@
 from models.objects.object import Object
 from colorama import init, Fore
-from typing import Optional
+from typing import Optional, Union
+from aiogram.types import Message, CallbackQuery
 
 init(autoreset=True)
 
 
 class User(Object):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        self.id: Optional[int] = None
-        self.username: Optional[str] = None
-        self.first_name: Optional[str] = None
-        self.last_name: Optional[str] = None
+        self.id: int = self.update.from_user.id
+        self.username: str = self.update.from_user.username
+        self.first_name: str = self.update.from_user.first_name
+        self.last_name: str = self.update.from_user.last_name
         self.new_member: bool = False  # TODO: когда будет готово подключение к БД добей сюда функцию
 
 
 class Member(User):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class Admin(Member):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class Creator(Member):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class NotMember(User):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
-class Kicked(NotMember):
-    def __init__(self):
-        super().__init__()
+class MemberKicked(NotMember):
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
-class Left(NotMember):
-    def __init__(self):
-        super().__init__()
+class MemberLeft(NotMember):
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
