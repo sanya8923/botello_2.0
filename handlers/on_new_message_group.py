74d9a4a489e1de130700ef9c2b21d0970d5d2d4c
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-04 00:59:43 +0400

    refactor on_new_message_from_admin_group

--- handlers/on_new_message_group.py
+++ handlers/on_new_message_group.py
@@ -4,7 +4,8 @@ from aiogram.types import Message
 
 from filters.user_role_filter import UserRoleFilter
 from filters.chat_type_filter import ChatTypeFilter
-from models.handlers.new_message_manager import NewMessageFromCreatorManager
+from models.handlers.new_message_manager import NewMessageFromCreatorManager, NewMessageFromAdminManager, \
+    NewMessageFromMemberManager
 
 
 router = Router()
@@ -22,6 +23,8 @@ async def on_new_message_from_creator_group(message: Message):
 @router.message(UserRoleFilter(user_role='administrator'))
 async def on_new_message_from_admin_group(message: Message):
     print(Fore.BLUE + 'on_new_message_from_admin_group')
+    manager = NewMessageFromAdminManager(message)
+    await manager.serialize()
 
 
 @router.message(UserRoleFilter(user_role='member'))
