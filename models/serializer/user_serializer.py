3176266f54f04778ebec224aca858146fe3b51dc
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-03 20:16:56 +0400

    refactor class UserSerializer

--- models/serializer/user_serializer.py
+++ models/serializer/user_serializer.py
@@ -1,5 +1,6 @@
 from models.serializer.serializer import Serializer
 from models.objects.user import User, Member, MiddleMember, NewMember, Admin, Creator
+from aiogram.types import Message
 from typing import Optional
 from colorama import init, Fore
 
@@ -7,19 +8,22 @@ init(autoreset=True)
 
 
 class UserSerializer(Serializer):
-    def __init__(self, user: User):
-        super().__init__(user)
+    def __init__(self):
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
-        self.user_id: int = user.id
-        self.username: str = user.username
-        self.user_first_name: str = user.first_name
-        self.user_last_name: str = user.last_name
-        self.user_status: Optional[str] = None
+        self.user_id: Optional[int] = None
+        self.username: Optional[str] = None
+        self.user_first_name: Optional[str] = None
+        self.user_last_name: Optional[str] = None
         self.file: str = 'jsons/user.json'
 
-    async def to_json(self):
+    async def to_json(self, message: Message):
         print(Fore.LIGHTYELLOW_EX + f'{self.to_json.__name__} in class {self.__class__.__name__}')
+
+        self.user_id = message.from_user.id
+        self.username = message.from_user.username
+        self.user_first_name = message.from_user.first_name
+        self.user_last_name = message.from_user.last_name
 
         data = {
             'id': self.user_id,
@@ -38,31 +42,31 @@ class UserSerializer(Serializer):
 
 
 class MemberSerializer(UserSerializer):
-    def __init__(self, member: Member):
-        super().__init__(member)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class MiddleMemberSerializer(MemberSerializer):
-    def __init__(self, member: MiddleMember):
-        super().__init__(member)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class NewMemberSerializer(MemberSerializer):
-    def __init__(self, member: NewMember):
-        super().__init__(member)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class AdminSerializer(UserSerializer):
-    def __init__(self, admin: Admin):
-        super().__init__(admin)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class CreatorSerializer(UserSerializer):
-    def __init__(self, creator: Creator):
-        super().__init__(creator)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
