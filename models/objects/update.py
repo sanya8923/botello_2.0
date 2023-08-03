f5ddf5cee7f5520d0ba61fe4e68ee115bb498fe9
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-03 22:40:27 +0400

    refactor update.py

--- models/objects/update.py
+++ models/objects/update.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Union
 from models.objects.object import Object
 from models.objects.user import User
 from models.objects.chat import Chat
@@ -9,40 +9,38 @@ init(autoreset=True)
 
 
 class UpdateData(Object):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        self.update: Optional[Message, CallbackQuery] = None
 
 
 class MessageData(UpdateData):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, message: Message):
+        super().__init__(message)
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        self.message_data: Optional[Message] = None
-        self.id: Optional[int] = None
-        self.from_user: Optional[User] = None
-        self.from_chat: Optional[Chat] = None
-        self.text: Optional[str] = None
-        self.entities_data: Optional[List[MessageEntity]] = None
+
+        self.id: int = self.update.message_id
+        self.from_user: User = self.update.from_user
+        self.from_chat: Chat = self.update.chat
+        self.text: str = self.update.text
+        self.entities_data: Optional[List[MessageEntity]] = self.update.entities
 
 
 class MessagePrivateChat(MessageData):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, message: Message):
+        super().__init__(message)
         print(Fore.BLUE + f'{self.__class__.__name__}')
         self.for_chat: Optional[List[Chat]] = None
 
 
 class MessagePublicChat(MessageData):
-    def __init__(self):
+    def __init__(self, message: Message):
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        super().__init__()
+        super().__init__(message)
 
 
 class Callback(UpdateData):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, callback: CallbackQuery):
+        super().__init__(callback)
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        self.callback: Optional[CallbackQuery] = None
-        self.callback_data: Optional[str] = None
+        self.callback_data: str = callback.data
