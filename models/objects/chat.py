1feab2f2e3e345adca636646fae828bd6b9c3d7c
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-03 22:39:55 +0400

    refactor chat.py

--- models/objects/chat.py
+++ models/objects/chat.py
@@ -1,39 +1,48 @@
 from models.objects.object import Object
 from colorama import init, Fore
-from typing import Optional
+from typing import Optional, Union
+from aiogram.types import Message, CallbackQuery
 
 init(autoreset=True)
 
 
 class Chat(Object):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        self.id: Optional[int] = None
+
+        if isinstance(self.update, Message):
+            self.id: int = self.update.message_id
+        else:
+            self.id: int = self.update.id
 
 
 class PrivateChat(Chat):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class PublicChat(Chat):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        self.username: Optional[str] = None
-        self.title: Optional[str] = None
+        if isinstance(self.update, Message):
+            self.username: str = self.update.chat.username
+            self.title: str = self.update.chat.title
+        else:
+            self.username: str = self.update.message.chat.username
+            self.title: str = self.update.message.chat.title
 
 
 class Group(PublicChat):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class SuperGroup(PublicChat):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, update: Union[Message, CallbackQuery]):
+        super().__init__(update)
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
