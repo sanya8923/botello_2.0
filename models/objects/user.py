7a998564420b086bdc24881194448991e371bd79
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-03 18:17:05 +0400

    refactor class User

--- models/objects/user.py
+++ models/objects/user.py
@@ -1,63 +1,62 @@
 from models.objects.object import Object
-from aiogram.types import Message
 from colorama import init, Fore
+from typing import Optional
 
 init(autoreset=True)
 
 
 class User(Object):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
-        self.id: int = message.from_user.id
-        self.username: str = message.from_user.username
-        self.first_name: str = message.from_user.first_name
-        self.last_name: str = message.from_user.last_name
-
+        self.id: Optional[int] = None
+        self.username: Optional[str] = None
+        self.first_name: Optional[str] = None
+        self.last_name: Optional[str] = None
 
 class Member(User):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class NewMember(Member):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class MiddleMember(Member):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class Admin(Member):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class Creator(Member):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class NotMember(User):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class Kicked(NotMember):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
 
 
 class Left(NotMember):
-    def __init__(self, message: Message):
-        super().__init__(message)
+    def __init__(self):
+        super().__init__()
         print(Fore.BLUE + f'{self.__class__.__name__}')
