7a3ff0fd8847a7424ba76271da4b97215227a4a8
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-03 23:22:01 +0400

    add class NewMessageManager

---
+++ models/handlers/new_message_manager.py
@@ -0,0 +1,24 @@
+from aiogram.types import Message
+from colorama import Fore, init
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from models.objects.user import Creator
+from models.serializer.user_serializer import CreatorSerializer
+
+from models.objects.chat import Group
+from models.serializer.chat_serializer import GroupSerializer
+
+from models.objects.update import MessagePublicChat
+from models.serializer.update_serializer import MessagePublicChatSerializer
+
+from models.serializer.group_member_role_serializer import GroupMemberRoleSerializer
+
+init(autoreset=True)
+
+
+class NewMessageManager(ABC):
+    def __init__(self, message: Message):
+        print(Fore.BLUE + f'{self.__class__.__name__}')
+        self.message = message
+
