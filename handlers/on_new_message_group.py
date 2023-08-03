2c74625b76ba33bf7b4b7f55f85ff7af21522796
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-03 23:21:28 +0400

    refactor def on_new_message_from_creator_group

--- handlers/on_new_message_group.py
+++ handlers/on_new_message_group.py
@@ -4,17 +4,8 @@ from aiogram.types import Message
 
 from filters.user_role_filter import UserRoleFilter
 from filters.chat_type_filter import ChatTypeFilter
+from models.handlers.new_message_manager import NewMessageFromCreatorManager
 
-from models.objects.user import Admin, Creator
-from models.serializer.user_serializer import AdminSerializer, CreatorSerializer
-
-from models.objects.chat import Group
-from models.serializer.chat_serializer import GroupSerializer
-
-from models.objects.update import MessagePublicChat
-from models.serializer.update_serializer import MessagePublicChatSerializer
-
-from models.serializer.group_member_role_serializer import GroupMemberRoleSerializer
 
 router = Router()
 router.message(ChatTypeFilter('group'))
@@ -24,19 +15,8 @@ init(autoreset=True)
 @router.message(UserRoleFilter(user_role='creator'))
 async def on_new_message_from_creator_group(message: Message):
     print(Fore.BLUE + 'on_new_message_from_creator_group')
-    message_data = MessagePublicChat(message)
-    creator = Creator(message)
-    group = Group(message)
-
-    message_serializer = MessagePublicChatSerializer()
-    creator_serializer = CreatorSerializer()
-    group_serializer = GroupSerializer()
-    group_member_role_serializer = GroupMemberRoleSerializer()
-
-    await message_serializer.to_json(message_data)  # return dict with message data and save json
-    await creator_serializer.to_json(creator)  # return dict with member data and save json
-    await group_serializer.to_json(group)  # return dict with group data and save json
-    await group_member_role_serializer.to_json(message_data)  # return dict with group_id, user_id and role and save json
+    manager = NewMessageFromCreatorManager(message)
+    await manager.serialize()
 
 
 @router.message(UserRoleFilter(user_role='administrator'))
