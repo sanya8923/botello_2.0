a41fc4a06237d351e60272d6a7143d4af8515d5e
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-04 21:52:59 +0400

    create class Db

--- models/objects/db.py
+++ models/objects/db.py
@@ -4,3 +4,15 @@ import config_reader
 
 cluster = motor.motor_asyncio.AsyncIOMotorClient(config_reader.config.mongo_db.get_secret_value())
 db = cluster['db']
+
+
+class Db:
+    def __init__(self):
+        self.mongo_db = db
+
+
+class MongoDb(Db):
+    def __init__(self):
+        super().__init__()
+        self.db = self.mongo_db
+
