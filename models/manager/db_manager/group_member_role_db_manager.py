d245d8ec8d1b97a85d02c73ddfd4a2072c40ea86
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-04 21:47:49 +0400

    create GroupMemberRoleDbManager and few methods

---
+++ models/manager/db_manager/group_member_role_db_manager.py
@@ -0,0 +1,36 @@
+from models.manager.db_manager.db_manager import DbManager
+
+
+class GroupMemberRoleDbManager(DbManager):
+    async def add(self):  # abstract method
+        pass
+
+    async def update(self):  # abstract method
+        pass
+
+    async def get(self):  # abstract method
+        pass
+
+    async def find_from_id(self):  # abstract method
+        pass
+
+    async def find_from_username(self):  # abstract method
+        pass
+
+
+class GroupMemberRoleMongoDbManager(GroupMemberRoleDbManager):
+    async def add(self):  # TODO: add method
+        pass
+
+    async def update(self):  # TODO: add method
+        pass
+
+    async def get(self):  # TODO: add method
+        pass
+
+    async def find_from_id(self):  # TODO: add method
+        pass
+
+    async def find_from_username(self):  # TODO: add method
+        pass
+
