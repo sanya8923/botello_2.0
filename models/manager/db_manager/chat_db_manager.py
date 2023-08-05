a832921bd5cf8a09adb23e74145c04151f398b24
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-05 19:37:00 +0400

    add to class ChatDbManager methods

--- models/manager/db_manager/chat_db_manager.py
+++ models/manager/db_manager/chat_db_manager.py
@@ -1,21 +1,72 @@
 from models.manager.db_manager.db_manager import DbManager
+from typing import Optional
+from motor.motor_asyncio import AsyncIOMotorDatabase
 
 
 class ChatDbManager(DbManager):
+    def __init__(self):
+        super().__init__()
+
     async def add(self):  # abstract method
+        pass
+
+    async def update(self):  # abstract method
+        pass
+
+    async def get(self):  # abstract method
+        pass
+
+    async def find_from_id(self):  # abstract method
+        pass
+
+    async def find_from_username(self):  # abstract method
         pass
 
 
 class PrivateChatDbManager(DbManager):
+    def __init__(self):
+        super().__init__()
+
     async def add(self):  # abstract method
+        pass
+
+    async def update(self):  # abstract method
+        pass
+
+    async def get(self):  # abstract method
+        pass
+
+    async def find_from_id(self):  # abstract method
+        pass
+
+    async def find_from_username(self):  # abstract method
         pass
 
 
 class PrivateChatMongoDbManager(PrivateChatDbManager):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    async def add(self):  # TODO: add method
+        pass
+
+    async def update(self):  # TODO: add method
+        pass
+
+    async def get(self):  # TODO: add method
+        pass
+
+    async def find_from_id(self):  # TODO: add method
+        pass
+
+    async def find_from_username(self):  # TODO: add method
+        pass
 
 
 class PublicChatDbManager(DbManager):
+    def __init__(self):
+        super().__init__()
+
     async def add(self):  # abstract method
         pass
 
@@ -33,6 +84,9 @@ class PublicChatDbManager(DbManager):
 
 
 class PublicChatMongoDbManager(PublicChatDbManager):
+    def __init__(self):
+        super().__init__()
+
     async def add(self):  # TODO: add method
         pass
 
