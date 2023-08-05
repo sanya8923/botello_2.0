9b8a9065b53b4f180751991c5d7797667e4f9af5
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-05 19:34:48 +0400

    refactor db.py

--- models/objects/db.py
+++ models/objects/db.py
@@ -1,15 +1,21 @@
 from abc import ABC, abstractmethod
 import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorDatabase
 import config_reader
+from typing import Optional, Type
+from colorama import init, Fore
+
+init(autoreset=True)
 
 
-cluster = motor.motor_asyncio.AsyncIOMotorClient(config_reader.config.mongo_db.get_secret_value())
+cluster = motor.motor_asyncio.AsyncIOMotorClient(config_reader.config.bot_token.get_secret_value())
 db = cluster['db']
 
 
 class Db(ABC):
     def __init__(self):
-        self.mongo_db = db
+        self.db: Optional[Type[Type[AsyncIOMotorDatabase]]]  # add new type when you add new databases
+        print(Fore.YELLOW + 'DB RUN')
 
     @abstractmethod
     async def plug(self):
@@ -19,7 +25,7 @@ class Db(ABC):
 class MongoDb(Db):
     def __init__(self):
         super().__init__()
-        self.db = self.mongo_db
+
 
     async def plug(self):
         pass
