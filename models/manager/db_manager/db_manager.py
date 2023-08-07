64cbce09554f47dd397840cd82cf55b02bed1941
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-08 01:18:41 +0400

    refactor db_manager.py

--- models/manager/db_manager/db_manager.py
+++ models/manager/db_manager/db_manager.py
@@ -1,36 +1,28 @@
-from abc import ABC, abstractmethod
-from typing import Optional, Type
-from motor.motor_asyncio import AsyncIOMotorDatabase
 from models.manager.manager import Manager
 import logger
 
 
-class DbManager(ABC, Manager):
+class DbManager(Manager):
     def __init__(self):
         super().__init__()
         self.db = None
 
-    @abstractmethod
     @logger.MyLogger(name='log').log_method_info
     async def add(self, *args, **kwargs):
         pass
 
-    @abstractmethod
     @logger.MyLogger(name='log').log_method_info
     async def update(self):
         pass
 
-    @abstractmethod
     @logger.MyLogger(name='log').log_method_info
     async def get(self):
         pass
-
-    @abstractmethod
+    
     @logger.MyLogger(name='log').log_method_info
     async def find_from_id(self):
         pass
-
-    @abstractmethod
+    
     @logger.MyLogger(name='log').log_method_info
     async def find_from_username(self):
         pass
