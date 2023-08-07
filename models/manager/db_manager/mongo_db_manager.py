2e77eb5423fedfd012f2c136d70ee3bd032c10d2
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-08-08 01:18:56 +0400

    refactor mongo_db_manager.py

--- models/manager/db_manager/mongo_db_manager.py
+++ models/manager/db_manager/mongo_db_manager.py
@@ -1,11 +1,13 @@
 from models.manager.db_manager.db_manager import DbManager
 import logger
+from models.objects.db import MongoDb
 
 
 class MongoDbManager(DbManager):
     def __init__(self):
         super().__init__()
-        self.db = None
+        self.mongo_db = MongoDb()
+        self.db = self.mongo_db.db
 
     @logger.MyLogger(name='log').log_method_info
     async def add(self, *args, **kwargs):
@@ -30,8 +32,10 @@ class MongoDbManager(DbManager):
 
 class MessageMongoDbManager(MongoDbManager):
     @logger.MyLogger(name='log').log_method_info
-    async def add(self, *args, **kwargs):
-        pass
+    async def add(self, data: dict, group_id: int):
+
+        collection = self.db[f'messages {group_id}']
+        await self.mongo_db.update(collection, data)
 
     @logger.MyLogger(name='log').log_method_info
     async def update(self):
@@ -52,8 +56,10 @@ class MessageMongoDbManager(MongoDbManager):
 
 class ChatMongoDbManager(MongoDbManager):
     @logger.MyLogger(name='log').log_method_info
-    async def add(self, *args, **kwargs):
-        pass
+    async def add(self, data: dict):
+
+        collection = self.db['groups']
+        await self.mongo_db.update(collection, data)
 
     @logger.MyLogger(name='log').log_method_info
     async def update(self):
@@ -74,8 +80,10 @@ class ChatMongoDbManager(MongoDbManager):
 
 class UserMongoDbManager(MongoDbManager):
     @logger.MyLogger(name='log').log_method_info
-    async def add(self, *args, **kwargs):
-        pass
+    async def add(self, data: dict):
+
+        collection = self.db['users']
+        await self.mongo_db.update(collection, data)
 
     @logger.MyLogger(name='log').log_method_info
     async def update(self):
@@ -97,8 +105,10 @@ class UserMongoDbManager(MongoDbManager):
 class GroupMemberRoleMongoDbManager(MongoDbManager):
 
     @logger.MyLogger(name='log').log_method_info
-    async def add(self, *args, **kwargs):
-        pass
+    async def add(self, data: dict):
+
+        collection = self.db['group_member_role']
+        await self.mongo_db.update_role(collection, data)
 
     @logger.MyLogger(name='log').log_method_info
     async def update(self):
